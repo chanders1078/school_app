0fe9fcf276768b062bb13c77ae94cadab3b79b4f
chanders1078 <shekharsaa@gmail.com>
2020-06-27 20:14:41 +0530

    uppernav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import './App.css';
 import {Header} from './components/Header';
-=======
->>>>>>> front
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
         <Header/>
-=======
->>>>>>> front
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,45 +5,25 @@ export class Header extends Component {
     render() {
         return (
             <div>
-<<<<<<< HEAD
-                <nav className="navbar navbar-expend-lg navbar-dark text-light  ">
-                    <span class="material-icons">phone 
-                        </span> <p>+9988785652</p>
-                    <p>icon2</p>
-                    <p>
-                        <a>Login</a>/<a>Signup</a>
-=======
-                <nav className="navbar navbar-expend-lg navbar-dark">
-                    <p>Icon</p>
-                    <p><i className="icon-phone"></i>9988785652</p>
-                    <p>icon2</p>
-                    <p>
-                        <a href="#">Login</a>/<a>Signup</a>
->>>>>>> front
-                    </p>
-                </nav>
-                <nav className="navbar  navbar-expand-lg navbar-light bg-light ">
-                    <a className="navbar-brand" href="#">Logo</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarcontent" >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarcontent">
-                        <ul className="navbar-nav  ml-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home<hr/></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">About<hr/></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Course<hr/></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Contact<hr/></a>
-                            </li>
-                        </ul>
+                <nav className="navbar">
+                    <div className="icon" >
+                        <span class="material-icons">
+                            call</span>+9988456545 
                     </div>
-
+                    <div className="icon">
+                    <span class="material-icons">
+                            email
+                            </span>School@gmail.com
+                    </div>
+                    <div className="icon">
+                        <span class="material-icons">
+                        login
+                        </span><a  className="upperlink"  href="#">Login/</a>
+                        <span class="material-icons">
+                        how_to_reg
+                        </span>
+                        <a  className="upperlink" href="#">Signup</a>
+                    </div>
                 </nav>
             </div>
         );
